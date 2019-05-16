5ead78bf5e0d505b33c54debac40afc3f6de8a77
soyoungpark9187 <44626132+soyoungpark9187@users.noreply.github.com>
2019-05-16 14:33:12 +0900

    1. set inputType as textPassword Password EditText in login page 2. set authority of user who makes the circle as manager

--- app/src/main/java/com/example/udongmp/CircleRegisterFormActivity.java
+++ app/src/main/java/com/example/udongmp/CircleRegisterFormActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -13,6 +16,8 @@ import java.util.Date;
 public class CircleRegisterFormActivity extends AppCompatActivity {
     Button bu_submit;
     EditText et_name,et_school;
+
+    private FirebaseAuth firebaseAuth;
 
     private DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
     private DatabaseReference circlrInfoRef = mRootRef.child("circle");
@@ -26,10 +31,17 @@ public class CircleRegisterFormActivity extends AppCompatActivity {
         et_school = findViewById(R.id.ET_school);
         et_name = findViewById(R.id.ET_name);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         bu_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
                 circlrInfoRef.child(et_school.getText().toString()+":"+et_name.getText().toString()+"/info").setValue(new CircleInfoForDB(et_name.getText().toString(),et_school.getText().toString(),new Date().toString()));
+                // 생성한 user manager로 등록
+                MemberInfoForDB memberInfoForDB= new MemberInfoForDB("manager",new Date().toString());
+                circlrInfoRef.child(et_school.getText().toString()+":"+et_name.getText().toString()+"/member").child(user.getUid()).setValue(memberInfoForDB);
+                Toast.makeText(CircleRegisterFormActivity.this, "동아리가 생성되었습니다!", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
